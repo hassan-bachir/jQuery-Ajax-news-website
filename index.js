0db4e98edac15d6940f2cb09310b276af935cfa2
hassan-bachir <125279202+hassan-bachir@users.noreply.github.com>
2023-03-12 20:27:19 +0200

    fixed database error

--- index.js
+++ index.js
@@ -1,19 +1,21 @@
-$('#result').html( $.ajax({
-    url: 'https://jsonplaceholder.typicode.com/posts',
-    method: 'POST',
-    headers: {
-      'Authorization': 'Bearer mytoken',
-      'Content-Type': 'application/json'
-    },
-    data: {
-      'name': 'John Doe',
-      'email': 'johndoe@example.com'
-    },
-    timeout: 5000,
-    success: function(response) {
-      console.log(response);
-    },
-    error: function(error) {
-      console.error(error);
-    }
-  }));
+$(document).ready(function() {
+	$.ajax({
+		url: 'news.php',
+		type: 'GET',
+		dataType: 'json',
+		success: function(data) {
+			var newsHTML = '';
+			for(var i = 0; i < data.length; i++) {
+				newsHTML += '<h2>' + data[i].title + '</h2>';
+				newsHTML += '<p>' + data[i].content + '</p>';
+				newsHTML += '<p class="date">' + data[i].date + '</p>';
+			}
+			$('#news').html(newsHTML);
+		},
+		error: function() {
+			alert('Error loading news.');
+		}
+	});
+});
+
+
